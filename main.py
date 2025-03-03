3398ddb694365759b8250530509d46de844cf0da
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-02 21:36:58 -0500

    Add user authentication and registration functionality to main.py and module_funcs.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print('hello world')
+from module_funcs import *
+
+def inicio():
+    print('Bienvenido a PyShop - Tienda Online 🛒')
+    print('Inicie Sesión o Regístrese')
+    print('1. Iniciar Sesión')
+    print('2. Registrarse')
+    print('3. Salir')
+    option = input('Opción: ')
+    if option == '1':
+        login_user()
+    elif option == '2':
+        add_user()
+    elif option == '3':
+        print('Gracias por visitarnos. Vuelva pronto.')
+    else:
+        print('Opción no válida. Intente nuevamente.')
+        inicio()
+
+if __name__ == '__main__':
+    inicio()
--- module_funcs.py
+++ module_funcs.py
@@ -1,4 +1,52 @@
+# Import libraries
 import pandas as pd
+import datetime as dt
+# Define DataFrames with the csv's data
 df_products = pd.read_csv('data/products.csv')
-df_users = pd.read_csv('data/users.csv')
+df_users = pd.read_csv('data/users.csv', dtype={'contraseña': str})
 df_orders = pd.read_csv('data/orders.csv')
+
+# Func add user
+def add_user():
+    global df_users
+    new_id = df_users['usuario_id'].max() + 1 if not df_users.empty else 1
+    name = input('Ingrese su nombre: ')
+    user = input('Ingrese su usuario: ')
+    password = input('Cree una contraseña: ')
+    email = input('Ingrese su email: ')
+    register_date = dt.datetime.now().strftime('%Y-%m-%d')
+
+    new_user = {
+        'usuario_id': new_id,
+        'nombre': name,
+        'usuario': user,
+        'contraseña': password,
+        'email': email,
+        'fecha_registro': register_date
+    }
+
+    df_users = pd.concat([df_users, new_user], ignore_index=True)
+    df_users.to_csv('data/users.csv', index=False)
+    print(f"✅ Usuario '{user}' agregado con éxito.")
+
+def login_user():
+    global df_users
+    att = 3
+    user = input('Ingrese su usuario: ')
+    password = input('Ingrese su contraseña: ')
+    user_data = df_users[(df_users['usuario'] == user) & (df_users['contraseña'] == password)]
+
+    while att > 1:
+        user_data = df_users[(df_users['usuario'] == user) & (df_users['contraseña'] == password)]
+        if not user_data.empty:
+            print(f"✅ Bienvenido {user_data['nombre'].values[0]}")
+            break
+        else:
+            print('❌ Usuario o contraseña incorrectos.')
+            att -= 1
+            print(f'Intentos restantes: {att}')
+            user = input('Ingrese su usuario: ')
+            password = input('Ingrese su contraseña: ')
+    if att == 0:
+        print('❌ Ha superado el número de intentos permitidos. Intente más tarde.')
+        return
