f0ac6a8f314e0d6fdafd3d88bdee75be64c81675
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-02 22:58:45 -0500

    Refactor inicio function to implement a loop for user login and registration options with improved feedback

--- main.py
+++ main.py
@@ -2,20 +2,28 @@ from module_funcs import *
 
 def inicio():
     print('Bienvenido a PyShop - Tienda Online 🛒')
-    print('Inicie Sesión o Regístrese')
-    print('1. Iniciar Sesión')
-    print('2. Registrarse')
-    print('3. Salir')
-    option = input('Opción: ')
-    if option == '1':
-        login_user()
-    elif option == '2':
-        add_user()
-    elif option == '3':
-        print('Gracias por visitarnos. Vuelva pronto.')
-    else:
-        print('Opción no válida. Intente nuevamente.')
-        inicio()
+    while True:
+        print('\nInicie Sesión o Regístrese')
+        print('1. Iniciar Sesión')
+        print('2. Registrarse')
+        print('3. Salir')
+        option = input('Opción: ')
+        if option == '1':
+            login_result = login_user()
+            if login_result['response']:
+                user_id = login_result['user_id']
+                menu(user_id)
+            else:
+                continue
+        elif option == '2':
+            add_user()
+        elif option == '3':
+            print('Gracias por visitarnos. Vuelva pronto.')
+            break
+        else:
+            print('❌ Opción no válida. Intente nuevamente.')
+            inicio()
+
 
 if __name__ == '__main__':
     inicio()
