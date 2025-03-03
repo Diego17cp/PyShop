2b29ea35fd8f2c4a62471178a4987cde43120e93
Diego17 <diegoalbertocastropastor@gmail.com>
2025-03-02 22:47:43 -0500

    Refactor login_user function to improve user feedback and attempt handling

--- module_funcs.py
+++ module_funcs.py
@@ -36,17 +36,23 @@ def login_user():
     password = input('Ingrese su contraseña: ')
     user_data = df_users[(df_users['usuario'] == user) & (df_users['contraseña'] == password)]
 
-    while att > 1:
+    while att > 0:
         user_data = df_users[(df_users['usuario'] == user) & (df_users['contraseña'] == password)]
         if not user_data.empty:
             print(f"✅ Bienvenido {user_data['nombre'].values[0]}")
-            break
+            return {
+                'response': True,
+                'user_id': user_data['usuario_id'].values[0],
+            }
         else:
-            print('❌ Usuario o contraseña incorrectos.')
             att -= 1
-            print(f'Intentos restantes: {att}')
-            user = input('Ingrese su usuario: ')
-            password = input('Ingrese su contraseña: ')
-    if att == 0:
-        print('❌ Ha superado el número de intentos permitidos. Intente más tarde.')
-        return
+            if att > 0:
+                print('❌ Usuario o contraseña incorrectos.')
+                print(f'Intentos restantes: {att}')
+                user = input('Ingrese su usuario: ')
+                password = input('Ingrese su contraseña: ')
+            else:
+                print('❌ Ha superado el número de intentos permitidos. Intente más tarde.')
+                return {
+                    'response': False,
+                }
